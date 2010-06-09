f09a1ba752ae578fcda656f5ff30cb81eb764146
Ryan Sonnek <ryan@socialcast.com>
2010-06-08 23:10:53 -0500

    lock down email_preview routes to only the supported environments

--- app/controllers/email_preview_controller.rb
+++ app/controllers/email_preview_controller.rb
@@ -1,6 +1,12 @@
 class EmailPreviewController < ApplicationController
   unloadable
+  before_filter :enforce_allowed_environments
+
   def show
     @mail = EmailPreview.emails[params[:id].to_sym]
+  end
+  private
+  def enforce_allowed_environments
+    raise "'#{Rails.env}' is not in the supported list of environments from EmailPreview.allowed_environments: #{EmailPreview.allowed_environments.inspect}" unless EmailPreview.allowed_environments.include?(Rails.env)
   end
 end
--- lib/email_preview.rb
+++ lib/email_preview.rb
@@ -5,7 +5,12 @@ module EmailPreview
 
   class << self
     attr_accessor :emails
+    attr_accessor :allowed_environments
 
+    def allowed_environments
+      @allowed_environments ||= ['development', 'test']
+      @allowed_environments
+    end
     def register(key, &block)
       mail = yield
       self.emails ||= {}
