17540eb3e32adf422ab30d3e8c74b3a6f48f0acb
Lars Klevan <lars@socialcast.com>
2012-10-09 14:30:29 -0500

    store the preview delivery email in the session

--- app/controllers/email_preview_controller.rb
+++ app/controllers/email_preview_controller.rb
@@ -6,6 +6,7 @@ class EmailPreviewController < ApplicationController
   before_filter :build_email, :only => [:show, :deliver, :details, :preview]
 
   def deliver
+    session[:email_preview_to] = params[:to]
     @mail.to params[:to]
     
     previous_delivery_method = ActionMailer::Base.delivery_method
